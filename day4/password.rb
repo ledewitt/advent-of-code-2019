acb424e25fc80a96b4503f5f371e6d89b134fd83
Luke DeWitt <luke@Lukes-iMac.local>
2019-12-05 20:45:12 -0700

    day 4 part 1

--- day4/password.rb
+++ day4/password.rb
@@ -6,13 +6,36 @@ def valid_passwod?(code)
 end
 
 def two_adj_d (code)
-  arr = code.to_s.split('')
-  arr.each do |k,idx|
+  arr = code.to_s.split('').map(&:to_i)
+  doubles = false
+  increasing_or_same = false
 
-    if k == arr[idx+1]
-      p "true"
-    end   
+  list = arr.map.with_index{|k,idx| [k,arr[idx+1]]}
+  list.pop
+
+  increasing_or_same = list.all? do |pair|
+    pair[0] <= pair[1]
+  end
+
+  doubles = list.any? do |pair|
+    pair[0] == pair[1]
+  end
+
+  if doubles == true && increasing_or_same == true
+    return true
+  else
+    return false
+  end
 end
 
 password = 111111
-two_adj_d(password)
+p two_adj_d(password)
+
+password = 223450
+p two_adj_d(password)
+
+value = (278384..824795).to_a.select do |test|
+  two_adj_d(test)
+end
+
+p value.size
